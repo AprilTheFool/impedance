dd8607f68f374f766eff132865f917c6cf9f9ce3
April <aprilhombsch@gmail.com>
2025-05-29 21:54:38 +1000

    removed FPSController

--- FPSController/weapon_manager/weapon_manager.gd
+++
@@ -1,8 +0,0 @@
-class_name WeaponManager
-extends Node3D
-
-@export var player : CharacterBody3D
-@export var bullet_raycast : RayCast3D
-
-@export var view_model_container : Node3D
-@export var world_model_container : Node3D
--- FPSController/weapon_manager/weapon_resource.gd
+++
@@ -1,4 +0,0 @@
-class_name WeaponResource
-extends Resource
-
-@export var damage = 10
