dd7ab0486e325de23a2c5ab4a8d3d61e91bf1e6b
April <aprilhombsch@gmail.com>
2025-05-31 01:59:30 +1000

    removed tp controller, started new fpv controller

---
+++ PlayerController/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody3D
+
+@export var look_sensitivity : float = 0.006
+
+func _ready():
+	## hide world model in fpv
+	for child in %WorldModel.find_children("*", "VisualInstance3D"):
+		child.set_layer_mask_value(1, false)
+		child.set_layer_mask_value(2, true)
+
+func _unhandled_input(event):
+	## capture mouse when window is clicked and release when esc is pressed
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+	## handle mouse movement
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			rotate_y(-event.relative.x * look_sensitivity)
+			%Camera3D.rotate_x(-event.relative.y * look_sensitivity)
+			%Camera3D.rotation.x = clamp(%Camera3D.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func _physics_process(delta):
+	pass
+
+func _process(delta):
+	pass
